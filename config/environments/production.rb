9ea25dff09c732bad35faefc7789aee49b3bae39
Far <farhood.fazeli@gmail.com>
2016-11-17 20:18:04 +0100

    upload to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,14 +85,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Required for heroku, set to actual host name
-  config.action_mailer.default_url_options = { host: 'helloiran.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Sets Paperclip to upload images to Amazon S3
   config.paperclip_defaults = {
   :storage => :s3,
-  :s3_region => ENV['AWS_REGION'],
   :s3_credentials => {
-    :bucket => ENV['S3_BUCKET'],
+    :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
